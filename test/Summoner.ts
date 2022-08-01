4e0f5d5ab6bbe5adde067fe6a6d73d1f15149e2a
hoangphuc <hoangphucnb97@gmail.com>
2022-08-01 13:29:49 +0100

    Add test for check grant role

--- test/Summoner.ts
+++ test/Summoner.ts
@@ -2,8 +2,9 @@ import { ethers, upgrades } from 'hardhat';
 import { expect } from 'chai';
 import { Summoner } from '../typechain-types';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { extendEnvironment } from 'hardhat/config';
 
-describe('FirstSaleMinter', async () => {
+context('FirstSaleMinter', async () => {
     let summoner: Summoner;
     let admin: SignerWithAddress, account1: SignerWithAddress;
     beforeEach(async () => {
@@ -12,24 +13,26 @@ describe('FirstSaleMinter', async () => {
         await summoner.deployed();
         await ethers.getSigners();
         [admin, account1] = await ethers.getSigners();
-    })
+    });
 
     it(`Deploy success`,async () => {
         expect(summoner.address).to.be.properAddress;
     });
 
-    it(`Revert if mint address is not minter`,async () => {
+    it(`Revert if mint address is not minter`, async () => {
         await expect(summoner.connect(account1).mint(account1.address, 1)).to.be.revertedWith("mint::not minter");
     });
 
-    describe(`Mint success a token`, async () => {
-        const minterRole = await summoner.MINTER_ROLE();
+    context(`Mint success a token`, async () => {
+        let minterRole: string;
         beforeEach(async() => {
+        minterRole = await summoner.MINTER_ROLE();
             await summoner.connect(admin).grantRole(minterRole, account1.address);
         });
 
         it(`Grant role success`, async () => {
-            await expect(summoner.hasRole(minterRole, account1.address)).to.be.equal(true);
+            const hasRole = await summoner.hasRole(minterRole, account1.address);
+            expect(hasRole).to.be.equal(true);
         });
     });
 })
