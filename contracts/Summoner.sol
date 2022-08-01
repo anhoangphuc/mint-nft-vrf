226e7f4d54f2bd2003273df4af45800c9d406a47
hoangphuc <hoangphucnb97@gmail.com>
2022-08-01 10:18:27 +0100

    First commit and add nft upgradeable

---
+++ contracts/Summoner.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+
+contract Summoner is ERC721Upgradeable, AccessControlUpgradeable {
+
+    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+
+    function initialize() public initializer {
+        __ERC721_init('Summoner', 'SMN');
+        __AccessControl_init();
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721Upgradeable, AccessControlUpgradeable) returns (bool) {
+        return ERC721Upgradeable.supportsInterface(interfaceId)
+            || AccessControlUpgradeable.supportsInterface(interfaceId);
+    }
+
+    function mint(address to, uint256 tokenId) external {
+        require(hasRole(MINTER_ROLE, msg.sender), 'mint:: not minter'); 
+        _safeMint(to, tokenId);
+    }
+}
