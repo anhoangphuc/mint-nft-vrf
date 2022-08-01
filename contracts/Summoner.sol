084cf2a5ede28ce4c0d323885b283e259798ca16
hoangphuc <hoangphucnb97@gmail.com>
2022-08-01 16:23:23 +0100

    Add test mint one token public mint

--- contracts/Summoner.sol
+++ contracts/Summoner.sol
@@ -5,24 +5,31 @@ import "./interfaces/ISummoner.sol";
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 
-contract Summoner is ERC721Upgradeable, AccessControlUpgradeable, ISummonerUpgradeable {
+contract Summoner is ERC721Upgradeable, ERC721EnumerableUpgradeable, AccessControlUpgradeable, ISummonerUpgradeable {
 
     bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
 
     function initialize() public initializer {
         __ERC721_init('Summoner', 'SMN');
+        __ERC721Enumerable_init();
         __AccessControl_init();
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721Upgradeable, AccessControlUpgradeable) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721Upgradeable, ERC721EnumerableUpgradeable, AccessControlUpgradeable) returns (bool) {
         return ERC721Upgradeable.supportsInterface(interfaceId)
+            || ERC721EnumerableUpgradeable.supportsInterface(interfaceId)
             || AccessControlUpgradeable.supportsInterface(interfaceId);
     }
 
     function mint(address to, uint256 tokenId) external {
         require(hasRole(MINTER_ROLE, msg.sender), 'mint::not minter'); 
         _safeMint(to, tokenId);
+    }
+    
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721Upgradeable, ERC721EnumerableUpgradeable) {
+        ERC721EnumerableUpgradeable._beforeTokenTransfer(from, to, tokenId);    
     }
 }
--- test/FirstSaleMinter.ts
+++ test/FirstSaleMinter.ts
@@ -1,22 +1,40 @@
 import { SignerWithAddress }from '@nomiclabs/hardhat-ethers/signers';
 import { ethers, upgrades } from 'hardhat';
-import { FirstSaleMinter } from '../typechain-types';
+import { FirstSaleMinter, Summoner } from '../typechain-types';
 import { expect } from 'chai';
 context(`FirstSaleMinter`, async () => {
     let admin: SignerWithAddress, account1: SignerWithAddress, account2: SignerWithAddress;
     let firstSaleMinter: FirstSaleMinter;
+    let summoner: Summoner;
     beforeEach(async () => {
-        const SummonerContract = await ethers.getContractFactory('Summoner');
-        const summoner = await upgrades.deployProxy(SummonerContract);
+        const SummonerContract = await ethers.getContractFactory('Summoner', admin);
+        summoner = await upgrades.deployProxy(SummonerContract) as Summoner;
         await summoner.deployed();
         [admin, account1, account2] = await ethers.getSigners();
         const FirstSaleMinterContract = await ethers.getContractFactory('FirstSaleMinter');
         firstSaleMinter = await upgrades.deployProxy(FirstSaleMinterContract, [summoner.address]) as FirstSaleMinter;
         await firstSaleMinter.deployed();
+
+        await summoner.connect(admin).grantRole(await summoner.MINTER_ROLE(), firstSaleMinter.address);
     });
 
     it('Deploy success', async () => {
         expect(firstSaleMinter.address).to.be.properAddress;
     });
 
+    context('Public mint success', async() => {
+        let femaleIndexStart: number;
+        beforeEach(async() => {
+            femaleIndexStart = await firstSaleMinter.FEMALE_INDEX_START();
+        });
+
+        it('Mint one token success', async() => {
+            await expect(firstSaleMinter.connect(account1).publicMint())
+            .to.be.emit(firstSaleMinter, 'PublicMint')
+            .to.be.emit(summoner, 'Transfer');
+
+            const newMintedToken = await summoner.tokenOfOwnerByIndex(account1.address, 0);
+            expect(newMintedToken).greaterThanOrEqual(0).lessThan(femaleIndexStart);
+        })
+    })
 })
