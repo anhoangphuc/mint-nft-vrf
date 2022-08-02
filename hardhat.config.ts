f501a6d02da4075fc8e2a5c98eeaf1183222e6ea
hoangphuc <hoangphucnb97@gmail.com>
2022-08-02 11:17:54 +0100

    Add mumbai network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,10 +2,22 @@ import { HardhatUserConfig } from "hardhat/config";
 import  "@openzeppelin/hardhat-upgrades";
 import "@nomicfoundation/hardhat-toolbox";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+const privateKey = process.env.PRIVATE_KEY || '';
+
 const config: HardhatUserConfig = {
   solidity: "0.8.12",
   mocha: {
     timeout: 4000000,
+  },
+  networks: {
+    mumbai: {
+      url: 'https://rpc-mumbai.matic.today/',
+      accounts: [privateKey],
+      chainId: 80001,
+    }  
   }
 };
 
