092bf1d8396c4751adfdafb093a4b1ffa20bed76
hoangphuc <hoangphucnb97@gmail.com>
2022-08-02 10:40:13 +0100

    Add test complete mint firstSaleMinterVrf

--- test/FirstSaleMinterVrf.ts
+++ test/FirstSaleMinterVrf.ts
@@ -28,4 +28,34 @@ context(`FirstSaleMinterVrf`, async () => {
     it(`Deploy success`, async () => {
         expect(firstSaleMinterVrf.address).to.be.properAddress;
     });
+
+    context(`Complete mint`, async() => {
+        let femaleIndexStart: number;
+        let whitelistMint: number;
+        let maleToken: number;
+        let femaleToken: number;
+        let publicMint: number;
+        beforeEach(async() => {
+            femaleIndexStart = await firstSaleMinterVrf.FEMALE_INDEX_START();
+            whitelistMint = await firstSaleMinterVrf.WHITELIST_MINT();
+            publicMint = await firstSaleMinterVrf.PUBLIC_MINT();
+            maleToken = await firstSaleMinterVrf.MALE_TOKEN();
+            femaleToken = await firstSaleMinterVrf.FEMALE_TOKEN();
+        });
+
+        it(`Complete mint success`, async() => {
+            const s = Array.from(Array(publicMint + whitelistMint).keys()).sort(() => Math.random() - 0.5);
+            for (const x of s) {
+                if (x < publicMint) {
+                    await firstSaleMinterVrf.connect(account1).mintPublic();
+                } else {
+                    await firstSaleMinterVrf.connect(account1).mintWhitelist();
+                }
+                const counter = await vrfCoordinator.counter();
+                await vrfCoordinator.connect(admin).rawFulfillRandomWords(counter.toNumber() - 1);
+            }
+            const balance = await summoner.balanceOf(account1.address);
+            expect(balance).to.be.equal(maleToken + femaleToken);
+        });
+    })
 })
