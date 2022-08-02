b214407ee82e7d36e53b56e95d9c68c7963c1b5b
hoangphuc <hoangphucnb97@gmail.com>
2022-08-02 10:58:18 +0100

    Add test mintPublic and mintWhitelist FirstSaleMinterVrf

--- test/FirstSaleMinterVrf.ts
+++ test/FirstSaleMinterVrf.ts
@@ -58,4 +58,97 @@ context(`FirstSaleMinterVrf`, async () => {
             expect(balance).to.be.equal(maleToken + femaleToken);
         });
     })
+
+    context('Public mint success', async() => {
+        let femaleIndexStart: number;
+        let maleToken: number;
+        let publicMint: number;
+        beforeEach(async() => {
+            femaleIndexStart = await firstSaleMinterVrf.FEMALE_INDEX_START();
+            maleToken = await firstSaleMinterVrf.MALE_TOKEN();
+            publicMint = await firstSaleMinterVrf.PUBLIC_MINT();
+        });
+
+        it('Mint one token success', async() => {
+            await firstSaleMinterVrf.connect(account1).mintPublic();
+            await expect(vrfCoordinator.connect(admin).rawFulfillRandomWords(0))
+            .to.be.emit(firstSaleMinterVrf, 'PublicMint')
+            .to.be.emit(summoner, 'Transfer');
+
+            const newMintedToken = await summoner.tokenOfOwnerByIndex(account1.address, 0);
+            expect(newMintedToken).greaterThanOrEqual(0).lessThan(femaleIndexStart);
+        });
+
+        it('Mint all public mint token success', async() => {
+            for (let i = 0; i < publicMint; i++) {
+                await firstSaleMinterVrf.connect(account1).mintPublic();
+            }
+            for (let i = 0; i < publicMint; i++) {
+                await vrfCoordinator.connect(admin).rawFulfillRandomWords(i);
+            }
+            const mintedTokens = new Set<number>();
+            for (let i = 0; i < publicMint; i++)
+                mintedTokens.add((await summoner.tokenOfOwnerByIndex(account1.address, i)).toNumber());
+            expect(mintedTokens.size).to.be.equal(publicMint);
+            expect(Array.from(mintedTokens).every((x) => x >= 0 && x < femaleIndexStart));
+        });
+
+        it(`Revert if mint public sale exceed`, async () => {
+            for (let i = 0; i < publicMint; i++) {
+                await firstSaleMinterVrf.connect(account1).mintPublic();
+            }
+            await expect(firstSaleMinterVrf.connect(account1).mintPublic())
+            .to.be.revertedWith('public::exceed');
+        })
+    })
+
+    context('Whitelist mint success', async() => {
+        let femaleIndexStart: number;
+        let whitelistMint: number;
+        let maleToken: number;
+        let femaleToken: number;
+        beforeEach(async() => {
+            femaleIndexStart = await firstSaleMinterVrf.FEMALE_INDEX_START();
+            whitelistMint = await firstSaleMinterVrf.WHITELIST_MINT();
+            maleToken = await firstSaleMinterVrf.MALE_TOKEN();
+            femaleToken = await firstSaleMinterVrf.FEMALE_TOKEN();
+        });
+
+        it('Whitelist mint token success', async() => {
+            await firstSaleMinterVrf.connect(account1).mintWhitelist();
+            await expect(vrfCoordinator.connect(admin).rawFulfillRandomWords(0))
+            .to.be.emit(firstSaleMinterVrf, 'WhitelistMint')
+            .to.be.emit(summoner, 'Transfer');
+
+            const balance = await summoner.balanceOf(account1.address);
+            expect(balance).to.be.equal(2);
+            const newMaleMintedToken = await summoner.tokenOfOwnerByIndex(account1.address, 0);
+            const newFemaleMintedToken = await summoner.tokenOfOwnerByIndex(account1.address, 1);
+            expect(newMaleMintedToken).greaterThanOrEqual(0).lessThan(femaleIndexStart);
+            expect(newFemaleMintedToken).greaterThanOrEqual(femaleIndexStart).lessThan(maleToken + femaleToken);
+        });
+
+        it('Mint all whitelist mint token success', async() => {
+            for (let i = 0; i < whitelistMint; i++) {
+                await firstSaleMinterVrf.connect(account1).mintWhitelist();
+            }
+            for (let i = 0; i < whitelistMint; i++) {
+                await vrfCoordinator.connect(admin).rawFulfillRandomWords(i);
+            }
+            const mintedTokens = new Set<number>();
+            for (let i = 0; i < whitelistMint * 2; i++)
+                mintedTokens.add((await summoner.tokenOfOwnerByIndex(account1.address, i)).toNumber());
+            expect(mintedTokens.size).to.be.equal(whitelistMint * 2);
+            expect(Array.from(mintedTokens).filter((x) => x >= 0 && x < femaleIndexStart).length).to.be.equal(whitelistMint);
+            expect(Array.from(mintedTokens).filter((x) => x >= femaleIndexStart && x < femaleToken + maleToken).length).to.be.equal(whitelistMint);
+        });
+
+        it(`Revert if mint whitelist sale exceed`, async () => {
+            for (let i = 0; i < whitelistMint; i++) {
+                await firstSaleMinterVrf.connect(account1).mintWhitelist();
+            }
+            await expect(firstSaleMinterVrf.connect(account2).mintWhitelist())
+            .to.be.revertedWith('whitelist::exceed');
+        });
+    });
 })
