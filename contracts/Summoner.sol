7e0968e5ecf93275fd749794c3cd625eb78f119d
hoangphuc <hoangphucnb97@gmail.com>
2022-08-03 10:41:18 +0100

    Add baseUri change support

--- contracts/Summoner.sol
+++ contracts/Summoner.sol
@@ -16,13 +16,16 @@ contract Summoner is
     ISummonerUpgradeable 
     {
     bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+    bytes32 public constant ADMIN_ROLE = keccak256('ADMIN_ROLE');
+    string private _baseUri;
 
-    function initialize() public initializer {
+    function initialize(string calldata baseUri_) public initializer {
         __ERC721_init('Summoner', 'SMN');
         __ERC721Enumerable_init();
         __ERC721Pausable_init();
         __AccessControl_init();
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _baseUri = baseUri_;
     }
 
     function supportsInterface(bytes4 interfaceId) public view override(ERC721Upgradeable, ERC721EnumerableUpgradeable, AccessControlUpgradeable) returns (bool) {
@@ -39,5 +42,18 @@ contract Summoner is
     function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721PausableUpgradeable) {
         ERC721PausableUpgradeable._beforeTokenTransfer(from, to, tokenId);
         ERC721EnumerableUpgradeable._beforeTokenTransfer(from, to, tokenId);    
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseUri;        
+    }
+
+    function baseURI() external view returns (string memory) {
+        return _baseURI();    
+    }
+
+    function setBaseUri(string memory newUri) external {
+        require(hasRole(ADMIN_ROLE, msg.sender), 'setBaseUri:not admin');        
+        _baseUri = newUri;
     }
 }
