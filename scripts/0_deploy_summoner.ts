3eb30a06b5ae256c721125cb91a91eaef2b8d3ac
hoangphuc <hoangphucnb97@gmail.com>
2022-08-03 11:08:04 +0100

    Update script to update summoner

--- scripts/0_deploy_summoner.ts
+++ scripts/0_deploy_summoner.ts
@@ -1,11 +1,12 @@
 import hre, { ethers, network, upgrades } from "hardhat";
-import { saveContract } from "./utils";
+import { getContracts, saveContract } from "./utils";
 
 (async function main() {
     console.log(`Start deploying Summoner contract`);
     const networkName = network.name;    
+    const baseUri = "ipfs://QmRjC7FrYHZLC2nJWPsSJjFX5cBHM58YZHAKmwDAJkGn8V/";
     const SummonerContract = await ethers.getContractFactory('Summoner');
-    const summoner = await upgrades.deployProxy(SummonerContract);
+    const summoner = await upgrades.deployProxy(SummonerContract, [baseUri]);
     await summoner.deployed();
 
     console.log(`Deploy summoner contract success at address ${summoner.address}`);
@@ -14,8 +15,9 @@ import { saveContract } from "./utils";
     console.log(`Saved contract address`);
 
     console.log(`Start verifying contract`);
+    const implementationAddress = await upgrades.erc1967.getImplementationAddress(summoner.address);
     await hre.run("verify:verify", {
-        address: summoner.address,
+        address: implementationAddress,
     });
     console.log(`Verify success`);
 })();
