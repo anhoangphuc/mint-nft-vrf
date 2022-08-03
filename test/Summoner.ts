4b64564d2f0ab0aef4f9300e32eedc041b4a8a01
hoangphuc <hoangphucnb97@gmail.com>
2022-08-03 10:55:27 +0100

    Add test for token uri

--- test/Summoner.ts
+++ test/Summoner.ts
@@ -4,12 +4,13 @@ import { expect } from 'chai';
 import { Summoner } from '../typechain-types';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 
-context('FirstSaleMinter', async () => {
+context('Summoner', async () => {
     let summoner: Summoner;
     let admin: SignerWithAddress, account1: SignerWithAddress, account2: SignerWithAddress;
+    let baseURI = 'BaseUri/'
     beforeEach(async () => {
         const SummonerContract = await ethers.getContractFactory('Summoner');
-        summoner = await upgrades.deployProxy(SummonerContract) as Summoner;
+        summoner = await upgrades.deployProxy(SummonerContract, [ baseURI ]) as Summoner;
         await summoner.deployed();
         await ethers.getSigners();
         [admin, account1, account2] = await ethers.getSigners();
@@ -26,7 +27,7 @@ context('FirstSaleMinter', async () => {
     context(`Mint success a token`, async () => {
         let minterRole: string;
         beforeEach(async() => {
-        minterRole = await summoner.MINTER_ROLE();
+            minterRole = await summoner.MINTER_ROLE();
             await summoner.connect(admin).grantRole(minterRole, account1.address);
         });
 
@@ -41,4 +42,37 @@ context('FirstSaleMinter', async () => {
             .withArgs(constants.AddressZero, account2.address, 1);
         })
     });
+
+    context(`Base uri check`, async() => {
+        beforeEach(async () => {
+            await summoner.connect(admin).grantRole(await summoner.ADMIN_ROLE(), account1.address);
+            await summoner.connect(admin).grantRole(await summoner.MINTER_ROLE(), account1.address);
+        })
+
+        it(`BaseUri correct`, async() => {
+            const contractUri = await summoner.baseURI();
+            expect(contractUri).to.be.equal(baseURI);
+
+            await summoner.connect(account1).mint(account1.address, 0);
+            const token0Uri = await summoner.tokenURI(0);
+            expect(token0Uri).to.be.equal(`${baseURI}0`);
+        });
+
+        it(`Revert if access non existed token`, async () => {
+            await expect(summoner.tokenURI(0))
+                .to.be.revertedWith("ERC721: invalid token ID");
+        })
+
+        it(`Change uri success`, async () => {
+            const newUri = 'NewURI';
+            await summoner.connect(account1).setBaseUri(newUri);
+            const contractUri = await summoner.baseURI();
+            expect(contractUri).to.be.equal(newUri);
+        });
+
+        it(`Revert if not admin change uri`, async () => {
+            await expect(summoner.connect(account2).setBaseUri(''))
+            .revertedWith('setBaseUri:not admin');
+        })
+    })
 })
